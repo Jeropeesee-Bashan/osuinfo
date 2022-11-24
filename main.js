7e517f65cd88e43ff81ad2f426d9f269bd312b63
Vinegret43 <fvinegret@mail.ru>
2022-11-24 16:12:20 +0300

    Enhance page transitions
    The way page content transitions were implemented was ok but there was another one which makes the code clearer.
    
    TODO: implement smooth page disappearance.

--- main.js
+++ main.js
@@ -1,48 +1,14 @@
-// Personal note: this js seems to be too mediocre.
-
 function show_page(id)
 {
-    let p;
-    let visible;
-
     const pages = document.getElementsByClassName("page-content-box");
 
-    for (let i = 0; i < pages.length; ++i) {
-        if (pages[i].id === id) {
-            p = pages[i];
-        }
-        if (pages[i].style.display === "flex") {
-            visible = pages[i];
-        }
+    for (let i = 0; i < pages.length; i++) {
+	if (pages[i].id == id) {
+	    pages[i].classList.replace("hidden-page", "shown-page");
+	}
+
+	else {
+	    pages[i].classList.replace("shown-page", "hidden-page");
+	}
     }
-
-    if (p == visible) return;
-
-    set_visible(visible, false);
-    setTimeout(function() {
-        set_visible(p, true);
-
-        for (let i = 0; i < pages.length; ++i) {
-            pages[i].style.display = pages[i] === p ? "flex" : "none";
-        }
-    }, 1000);
-}
-
-function set_visible(page, reverse)
-{
-
-    let start = Date.now();
-
-    let timer = setInterval(function() {
-        const timePassed = Date.now() - start;
-
-        if (timePassed >= 1000) {
-            clearInterval(timer);
-            return;
-        }
-
-        const step = timePassed / 1000;
-        page.style.opacity = reverse ? step : 1 - step;
-
-    }, 5);
 }
