da4e3018c3eeba6898006fb5a4c239ea5686498a
Andrey Kulenko <ankube904@gmail.com>
2022-11-24 17:47:48 +0300

    Fix indentation and change naming
    I'm just more comfortable with that!

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function show_page(id)
 {
-  let to_show = document.getElementById(id);
-  let shown_page = document.getElementsByClassName("shown-page")[0];
-  if (to_show != shown_page) {
-    shown_page.classList.replace("shown-page", "disappearing-page");
-    setTimeout(() => {
-      shown_page.classList.replace("disappearing-page", "hidden-page");
-      to_show.classList.replace("hidden-page", "shown-page");
-    }, 500)
-  }
+    const this_page = document.getElementById(id);
+    const that_page = document.getElementsByClassName("shown-page")[0];
+    
+    if (this_page != that_page) {
+	that_page.classList.replace("shown-page", "disappearing-page");
+	setTimeout(() => {
+	    that_page.classList.replace("disappearing-page", "hidden-page");
+	    this_page.classList.replace("hidden-page", "shown-page");
+	}, 500)
+    }
 }
